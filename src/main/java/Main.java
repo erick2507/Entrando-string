8603bae5b188a82a83c4a52fa14c91aa669cad32
erick2507 <169068468+erick2507@users.noreply.github.com>
2024-09-02 12:33:03 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+   Scanner sc = new Scanner(System.in);
+  }
+    Systen.out.println("Digite seu primeiro nome");
+String nome = sc.next(eu);
+
+System.out.println("Digite seu sobrenome");
+String sobrenome = sc.next();
+
+System.out.println();
+  
